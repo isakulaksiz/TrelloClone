84618c5ca2d3ab0695a4d3e2481dc6a5e7f6cb92
isakulaksiz <isakulaksiz@outlook.com>
2022-02-15 17:00:04 +0300

    #4 add signup xml layout

--- app/src/main/java/com/example/projemanage/IntroActivity.kt
+++ app/src/main/java/com/example/projemanage/IntroActivity.kt
@@ -1,10 +1,13 @@
 package com.example.projemanage
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
+import android.widget.Button
 
 class IntroActivity : AppCompatActivity() {
+    lateinit var _btnSignUpIntro: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intro)
@@ -13,5 +16,10 @@ class IntroActivity : AppCompatActivity() {
             WindowManager.LayoutParams.FLAG_FULLSCREEN,
             WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
+
+        _btnSignUpIntro = findViewById<Button>(R.id.btn_sign_up_intro)
+        _btnSignUpIntro.setOnClickListener{
+            startActivity(Intent(this,SignupActivity::class.java))
+        }
     }
 }
---
+++ app/src/main/java/com/example/projemanage/SignupActivity.kt
@@ -0,0 +1,31 @@
+package com.example.projemanage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import androidx.appcompat.widget.Toolbar
+
+class SignupActivity : AppCompatActivity() {
+    var _tbSignUp: Toolbar? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        _tbSignUp = findViewById<Toolbar>(R.id.toolbar_sign_up_activity)
+        setupActionBar()
+    }
+
+    private fun setupActionBar(){
+        setSupportActionBar(_tbSignUp)
+
+        val actionBar = supportActionBar
+        if(actionBar!=null){
+            actionBar.setDisplayHomeAsUpEnabled(true)
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_black_color_back_24dp)
+        }
+        _tbSignUp?.setNavigationOnClickListener{onBackPressed()} // geri gitme
+    }
+}

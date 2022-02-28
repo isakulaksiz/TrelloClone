9fe91bfd832a6a7ae746443e03a20ff741bf999f
isakulaksiz <isakulaksiz@outlook.com>
2022-02-28 15:23:57 +0300

    [#10]: bug fixed

--- app/src/main/java/com/example/projemanage/activities/MainActivity.kt
+++ app/src/main/java/com/example/projemanage/activities/MainActivity.kt
@@ -1,17 +1,18 @@
 package com.example.projemanage.activities
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.bumptech.glide.Glide
-
 import com.example.projemanage.R
+import com.example.projemanage.databinding.NavHeaderMainBinding
 import com.example.projemanage.firebase.FireStore
 import com.example.projemanage.models.User
 import com.google.android.material.navigation.NavigationView
@@ -22,16 +23,14 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
     private lateinit var toolbar_main_activity: Toolbar
     private lateinit var drawer_layout: DrawerLayout
     private lateinit var nav_view: NavigationView
-    private lateinit var nav_user_image: CircleImageView
-    private lateinit var tv_username: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
         toolbar_main_activity = findViewById<Toolbar>(R.id.toolbar_main_activity)
         drawer_layout = findViewById<DrawerLayout>(R.id.drawer_layout)
         nav_view = findViewById<NavigationView>(R.id.nav_view)
-        nav_user_image = findViewById<CircleImageView>(R.id.nav_user_image)
-        tv_username = findViewById<TextView>(R.id.tv_username)
 
         setUpActionBar()
         nav_view.setNavigationItemSelectedListener(this)
@@ -81,15 +80,33 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         return true
     }
 
+    /*
     fun updateNavigationUserDetails(user: User) {
-
         Glide
             .with(this)
             .load(user.image)
             .centerCrop()
             .placeholder(R.drawable.ic_user_place_holder)
-            .into(nav_user_image)
-        tv_username.text = user.name
+            .into(binding.navViewUserImage)
+        binding.tvUsername.text= user.name
+        Log.e("tv_username.text", tv_username.text.toString())
+        Log.e("user.image", user.image)
+    }
+    */
 
+    fun updateNavigationUserDetails(user: User) {
+        val headerView = nav_view.getHeaderView(0)
+
+        val navUserImage = headerView.findViewById<ImageView>(R.id.nav_view_user_image)
+
+        Glide
+            .with(this@MainActivity)
+            .load(user.image)
+            .centerCrop()
+            .placeholder(R.drawable.ic_user_place_holder)
+            .into(navUserImage)
+
+        val navUsername = headerView.findViewById<TextView>(R.id.tv_username)
+        navUsername.text = user.name
     }
 }

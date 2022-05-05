07b3268437ca518c58186b00dee4a9af5df4bcc9
isakulaksiz <isakulaksiz@outlook.com>
2022-05-05 12:26:59 +0300

    [#20]: implements item boards adapter and ui elements getBoardsList() populateBoardsListToUI()

--- app/src/main/java/com/example/trelloclone/activities/MainActivity.kt
+++ app/src/main/java/com/example/trelloclone/activities/MainActivity.kt
@@ -5,14 +5,20 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.MenuItem
+import android.view.View
 import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.trelloclone.R
+import com.example.trelloclone.adapters.BoardItemsAdapter
 import com.example.trelloclone.firebase.FireStore
+import com.example.trelloclone.models.Board
 import com.example.trelloclone.models.User
 import com.example.trelloclone.utils.Constants
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -24,6 +30,8 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
     private lateinit var drawer_layout: DrawerLayout
     private lateinit var nav_view: NavigationView
     private lateinit var fab_create_board: FloatingActionButton
+    private lateinit var rv_boards_list: RecyclerView
+    private lateinit var tv_no_boards_available: TextView
 
     private lateinit var _userName: String
     companion object{
@@ -38,19 +46,39 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         drawer_layout = findViewById<DrawerLayout>(R.id.drawer_layout)
         nav_view = findViewById<NavigationView>(R.id.nav_view)
         fab_create_board = findViewById(R.id.fab_create_board)
+        rv_boards_list = findViewById(R.id.rv_board_list)
+        tv_no_boards_available = findViewById(R.id.tv_no_boards_available)
+
 
         setUpActionBar()
         nav_view.setNavigationItemSelectedListener(this)
-        FireStore().loadUserData(this)
+        FireStore().loadUserData(this, true)
 
         fab_create_board.setOnClickListener{
             val intent = Intent(this, CreateBoardActivity::class.java)
             intent.putExtra(Constants.NAME, _userName)
             startActivity(intent)
-            //startActivity(Intent(this, CreateBoardActivity::class.java))
         }
     }
 
+    fun populateBoardsListToUI(boardsList: ArrayList<Board>){
+        hideProhgressDialog()
+
+        if(boardsList.size > 0){
+            rv_boards_list.visibility = View.VISIBLE
+            tv_no_boards_available.visibility = View.GONE
+
+            rv_boards_list.layoutManager = LinearLayoutManager(this)
+            // adapter değişiklikleri recyclerview'in boyutunu etkilemiyorsa - true
+            rv_boards_list.setHasFixedSize(true)
+
+            val adapter = BoardItemsAdapter(this, boardsList)
+            rv_boards_list.adapter = adapter
+        }else{
+            rv_boards_list.visibility = View.GONE
+            tv_no_boards_available.visibility = View.VISIBLE
+        }
+    }
     private fun setUpActionBar(){
         setSupportActionBar(toolbar_main_activity)
         toolbar_main_activity.setNavigationIcon(R.drawable.ic_action_navigation_menu)
@@ -103,7 +131,7 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         return true
     }
 
-    fun updateNavigationUserDetails(user: User) {
+    fun updateNavigationUserDetails(user: User, readBoardsList: Boolean) {
         val headerView = nav_view.getHeaderView(0)
         _userName = user.name
         val navUserImage = headerView.findViewById<ImageView>(R.id.nav_view_user_image)
@@ -117,5 +145,10 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
 
         val navUsername = headerView.findViewById<TextView>(R.id.tv_username)
         navUsername.text = user.name
+
+        if(readBoardsList){
+            showProgressDialog("Please wait!")
+            FireStore().getBoardsList(this)
+        }
     }
 }
